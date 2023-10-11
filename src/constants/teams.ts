7a06ceef6888885a2446b3d77b8f6d104f9cf940
noahnovickf <nnovickfaille@gmail.com>
2023-10-10 17:11:31 -0700

    add prem team type

--- src/constants/teams.ts
+++ src/constants/teams.ts
@@ -1,7 +1,7 @@
 type TeamAbreviation = {
   [key: string]: string;
 };
-
+// current Teams in the Premier League, which will be mapped for the UI, will not contain all Team constants as there are teams now in the Championship and League One
 export const teamAbreviation: TeamAbreviation = {
   ars: "Arsenal",
   avl: "Aston Villa",
@@ -21,6 +21,40 @@ export const teamAbreviation: TeamAbreviation = {
   nfo: "Nottingham Forest",
   shu: "Sheffield United",
   tot: "Tottenham Hotspur",
-  whu: "West Ham United",
+  whu: "West Ham",
   wol: "Wolverhampton Wanderers",
 };
+
+export type PremierLeagueTeam =
+  | "Arsenal"
+  | "Aston Villa"
+  | "Bournemouth"
+  | "Brighton & Hove Albion"
+  | "Burnley"
+  | "Chelsea"
+  | "Crystal Palace"
+  | "Everton"
+  | "Fulham"
+  | "Leeds United"
+  | "Liverpool"
+  | "Manchester City"
+  | "Manchester United"
+  | "Newcastle United"
+  | "Sheffield United"
+  | "Southampton"
+  | "Tottenham Hotspur"
+  | "West Bromwich Albion"
+  | "West Ham"
+  | "Wolverhampton Wanderers"
+  | "Norwich City"
+  | "Watford"
+  | "Brentford"
+  | "Leicester"
+  | "Middlesbrough"
+  | "Stoke"
+  | "Swansea"
+  | "Nottingham Forest"
+  | "Huddersfield"
+  | "Queens Park Rangers"
+  | "Hull";
+// to be continued
